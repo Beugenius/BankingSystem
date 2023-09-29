759957e76cc3f951a761ce0ca54614d55aee13fe
Bryce Hutchinson <brycehutchinson@mail.weber.edu>
2023-09-29 06:49:34 -0600

    database file almost implemented

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
+var MySQLStore = require("express-mysql-session")(session);
 
 var indexRouter = require('./routes/index');
 var customerAccountsRouter = require('./routes/customer/accounts');
@@ -27,6 +29,31 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap-icons')));
+
+// this will setup the database if it doesn't already exist
+var dbCon = require("./lib/database");
+// Session management to store cookies in a mysql server (has bug, so we assist by creating the database for it)
+var dbSessionPool = require("./lib/sessionPool");
+var sessionStore = new MySQLStore({}, dbSessionPool);
+// necessary middleware to store session cookies in mysql
+app.use(
+  session({
+    key: "session_cookie_name",
+    secret: "session_cookie_secret1234",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: "strict",
+    },
+  })
+);
+
+// middleware to make session variables available in .ejs template files 
+app.use((req, res, next) => {
+  res.locals.session = req.session; 
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/customer/accounts', customerAccountsRouter);
